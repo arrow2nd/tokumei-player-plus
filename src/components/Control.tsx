3840bd15df45bf31f1efe6433616ad4f31d07ce6
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-03-05 22:45:11 +0900

    remove: 再生操作

--- src/components/Control.tsx
+++ src/components/Control.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PlayControl from './PlayControl'
 import Select from './Select'
 import SeekBar from './SeekBar'
@@ -8,38 +8,32 @@ import openBrowserIcon from '../images/open_in_browser-white-24dp.svg'
 import shuffleIcon from '../images/shuffle-white-24dp.svg'
 
 const Control = (): JSX.Element => {
-  const [
-    isPlaying,
-    currentTime,
-    play,
-    pause,
-    setSrc,
-    setCurrentTime
-  ] = useAudio()
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [url, setUrl] = useState('')
+  const [currentTime, play, pause, resume, setCurrentTime] = useAudio()
 
-  const handleSetSrc = (url: string) => {
-    setSrc('https://omocoro.heteml.net/radio/' + url)
+  const changeUrl = (url: string) => {
+    setUrl('https://omocoro.heteml.net/radio/' + url)
   }
 
   const handlePlay = () => {
-    play()
-  }
-
-  const handlePause = () => {
-    pause()
-  }
-
-  const handleSeek = (ctime: number) => {
-    setCurrentTime(ctime)
+    play(url)
   }
 
   return (
     <>
-      <Select setSrc={handleSetSrc} />
+      <Select isPlaying={isPlaying} setUrl={changeUrl} />
       <SeekBar />
       <div className="control">
         <input type="image" src={openBrowserIcon} />
-        <PlayControl onPlayClick={handlePlay} onPauseClick={handlePause} />
+        <PlayControl
+          isPlaying={isPlaying}
+          setIsPlaying={(f: boolean) => setIsPlaying(f)}
+          url={url}
+          onNewPlay={handlePlay}
+          onResume={resume}
+          onPause={pause}
+        />
         <input type="image" src={shuffleIcon} />
       </div>
     </>
