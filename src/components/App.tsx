e30e93be786c3fb4e8aa516726fc80741ad8b2ed
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-03-08 21:04:56 +0900

    clean

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import TitleBar from './TitleBar'
-import Control from './Control'
+import UI from './UI'
 import '../styles/App.css'
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
       <TitleBar />
-      <Control />
+      <UI />
     </div>
   )
 }
