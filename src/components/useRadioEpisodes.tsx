4d89b56ca24ade3e52701ea515f0c0cbcc4f1aea
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-03-08 15:11:22 +0900

    change: 取得した値をキャッシュするよう変更

--- src/components/useRadioEpisodes.tsx
+++ src/components/useRadioEpisodes.tsx
@@ -1,4 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const cache: { [s: string]: number } = {}
+
+/**
+ * 最小値～最大値が格納された配列を作成
+ *
+ * @param min 最小値
+ * @param max 最大値
+ * @returns 数値配列
+ */
+function createArray(min: number, max: number): number[] {
+  return min === 0
+    ? [...Array(max + 1).keys()]
+    : [...Array(max).keys()].map((i) => ++i)
+}
 
 /**
  * 最新回の番号を取得
@@ -34,41 +49,32 @@ export const useRadioEpisodes = (
   last: number,
   tag: string,
   regex: string
-): JSX.Element[] => {
-  const [opts, setOpts] = useState([] as JSX.Element[])
-
-  const createOptions = (min: number, max: number): JSX.Element[] => {
-    const epOpts: JSX.Element[] = []
-    for (let i = min; i <= max; i++) {
-      epOpts.push(
-        <option key={i} value={i}>
-          {'# ' + String(i).padStart(3, '0')}
-        </option>
-      )
-    }
-    return epOpts
-  }
+): number[] => {
+  const [numArray, setNumArray] = useState([] as number[])
 
   useEffect(() => {
-    // 更新終了済みのラジオ
+    // 更新終了済みなら受け取った値をそのまま使う
     if (last !== 0) {
-      setOpts(createOptions(first, last))
+      setNumArray(createArray(first, last))
       return
     }
 
-    // TODO: ここでキャッシュが存在するかチェック。あればそれを返す
+    // 取得済みなら一時キャッシュから値を取得
+    if (tag in cache) {
+      console.log(`[cache] max:${cache[tag]}`)
+      setNumArray(createArray(first, cache[tag]))
+      return
+    }
 
     // 最新回を取得
     getLatestRadioNum(tag, regex)
       .then((max) => {
-        console.log(`get f:${first} m:${max}`)
-
-        // TODO: ここで取得した最新回の数値をキャッシュ
-
-        setOpts(createOptions(first, max))
+        console.log(`[get] first:${first} max:${max}`)
+        cache[tag] = max
+        setNumArray(createArray(first, max))
       })
       .catch((err) => console.error(err))
   }, [first, last, tag, regex])
 
-  return opts
+  return numArray
 }
